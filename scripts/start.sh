dc83dd79a65e4c654af71277082c79d3520132ba
sanadkhan-nextloop <skhan@nextlooptechnologies.com>
2025-07-15 16:57:28 +0530

    ADDED pm2 restart script for CICD

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,3 +8,7 @@ cd /home/ec2-user/workspace/lavisha-dev
 npm install
 npm run db:setup
 npm run db:seed
+npm run build
+
+pm2 delete lavisha-dev2>/dev/null
+pm2 start dist/src/main.js --name lavisha-dev 
