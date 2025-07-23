e2548ce86dc76da57550c7f8f36bb0482ad8460b
sanadkhan-nextloop <skhan@nextlooptechnologies.com>
2025-07-23 18:24:06 +0530

    MODIFIED start script for t3a.small instance

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,26 +15,14 @@ else
   echo "No dependency changes."
 fi
 
-# npm run db:setup
-# ✅ Run db:setup only if there are new migrations
-if git diff --name-only origin/staging -- prisma/migrations | grep -q .; then
-  echo "Running DB setup..."
-  npm run db:setup
-else
-  echo "No new migrations. Skipping DB setup."
-fi
+echo "Running DB setup..."
+npm run db:setup
 
 # use SKIP_SEED FLAG
 npm run db:seed
 
-# npm run db:generate
-# ✅ Run db:generate if prisma/schema.prisma has changed
-if git diff --name-only origin/staging -- prisma/schema.prisma | grep -q .; then
-  echo "Running db:generate..."
-  npm run db:generate
-else
-  echo "No schema changes. Skipping db:generate."
-fi
+echo "Running db:generate..."
+npm run db:generate
 
 load=$(uptime | awk -F'load average: ' '{ print $2 }' | cut -d, -f1 | xargs)
 if (( $(echo "$load > 1.0" | bc -l) )); then
@@ -42,12 +30,13 @@ if (( $(echo "$load > 1.0" | bc -l) )); then
   exit 1
 fi
 
-if [ ! -d "dist" ]; then
-  echo "No dist directory. Running build..."
-  npm run build
-else
-  echo "dist/ already exists. Skipping build."
-fi
+npm run build
+# if [ ! -d "dist" ]; then
+#   echo "No dist directory. Running build..."
+#   npm run build
+# else
+#   echo "dist/ already exists. Skipping build."
+# fi
 
 pm2 delete lavisha-dev 2>/dev/null
 pm2 start dist/src/main.js --name lavisha-dev 
